b5e7f7740feb91351dea93d9c31596355c7308df
Pavel Sobolev <paveloom@mail.ru>
2020-10-23 10:23:41 +0300

    Upload the changed `deps` on this branch.

---
+++ deps/get_author_title.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "glib.h"
+#include "poppler.h"
+
+void get_author_title(
+    const char *file,
+    char **author,
+    char **title
+)
+{
+
+    // load the document
+    PopplerDocument *document = poppler_document_new_from_gfile(
+        g_file_new_for_path(file), NULL, NULL, NULL
+    );
+
+    *author = poppler_document_get_author(document);
+    *title = poppler_document_get_title(document);
+
+    return;
+}
