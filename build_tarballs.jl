8f7edde19ea5127884e4c1bdf3b6e52060f9e2f6
Pavel Sobolev <paveloom@mail.ru>
2020-10-25 17:59:33 +0300

    Try to use new `Poppler_jll` artifacts.

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -3,42 +3,15 @@
 using BinaryBuilder, Pkg
 
 name = "PDFHighlights"
-version = v"0.1.0"
+version = v"0.1.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://poppler.freedesktop.org/poppler-0.87.0.tar.xz", "6f602b9c24c2d05780be93e7306201012e41459f289b8279a27a79431ad4150e"),
 	GitSource("https://github.com/paveloom-j/PDFHighlights.jl.git", "42fc05c2b21d7359d13a38daf252045a79ab55c5"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/poppler-*/
-
-# Create link ${bindir} before starting.  `OpenJPEGTargets.cmake` will try to
-# look for some executables in `sys-root/usr/local/bin`
-ln -s ${bindir} /opt/${target}/${target}/sys-root/usr/local/bin
-export CXXFLAGS="-I${prefix}/include/openjpeg-2.3"
-
-if [[ "${target}" == "${MACHTYPE}" ]]; then
-    # When building for the host platform, the system libexpat is picked up
-    rm /usr/lib/libexpat.so*
-fi
-
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix \
-    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_GTK_TESTS=OFF \
-    -DENABLE_CMS=lcms2 \
-    -DENABLE_GLIB=ON \
-    -DENABLE_QT5=OFF \
-    -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
-    -DWITH_GObjectIntrospection=OFF \
-    ..
-make -j${nproc}
-make install
-
 cd $WORKSPACE/srcdir/PDFHighlights.jl/deps/
 
 CC="gcc"
@@ -73,20 +46,6 @@ for file in ${OBJECTS[@]}; do
 done
 
 ${CC} ${LIB_FLAGS} -o ${libdir}/${LIB_NAME}.${LIB_EXT} ${OBJECTS_O[@]} ${LINK_FLAGS}
-
-# if [[ "${target}" == *-darwin* ]]; then
-#     gcc -std=c99 -g -O3 -fPIC -c get_author_title.c -o get_author_title.o `pkg-config --cflags poppler-glib`
-#     gcc -std=c99 -g -O3 -fPIC -c get_lines_comments_pages.c -o get_lines_comments_pages.o `pkg-config --cflags poppler-glib`
-#     gcc -dynamiclib -o $libdir/PDFHighlightsWrapper.dylib get_author_title.o get_lines_comments_pages.o -lgio-2.0 `pkg-config --libs poppler-glib`
-# elif [[ "${target}" == *-mingw* ]]; then
-#     gcc -std=c99 -g -O3 -c get_author_title.c -o get_author_title.o `pkg-config --cflags poppler-glib`
-#     gcc -std=c99 -g -O3 -c get_lines_comments_pages.c -o get_lines_comments_pages.o `pkg-config --cflags poppler-glib`
-#     gcc -shared -o $libdir/PDFHighlightsWrapper.dll get_author_title.o get_lines_comments_pages.o -L/workspace/destdir/bin -lgio-2.0 `pkg-config --libs poppler-glib` -Wl,--out-implib,$libdir/PDFHighlightsWrapper.a
-# else
-#     gcc -std=c99 -g -O3 -fPIC -c get_author_title.c -o get_author_title.o `pkg-config --cflags poppler-glib`
-#     gcc -std=c99 -g -O3 -fPIC -c get_lines_comments_pages.c -o get_lines_comments_pages.o `pkg-config --cflags poppler-glib`
-#     gcc -shared -Wl,--no-undefined -o $libdir/PDFHighlightsWrapper.so get_author_title.o get_lines_comments_pages.o `pkg-config --libs poppler-glib`
-# fi
 """
 
 # These are the platforms we will build for by default, unless further
@@ -100,16 +59,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency("Xorg_xorgproto_jll"),
-    Dependency("Cairo_jll"),
-    Dependency("Fontconfig_jll"),
-    # Dependency("GTK3_jll"),
+    Dependency("Poppler_jll"),
     Dependency("Glib_jll"),
-    Dependency("JpegTurbo_jll"),
-    Dependency("Libtiff_jll"),
-    Dependency("OpenJpeg_jll"),
-    # Dependency("gdk_pixbuf_jll"),
-    Dependency("libpng_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
