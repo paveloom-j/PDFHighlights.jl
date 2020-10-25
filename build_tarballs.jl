318a51f88918c7f04057227de529f3e8ac0161b0
Pavel Sobolev <paveloom@mail.ru>
2020-10-25 23:14:06 +0300

    Try with `DENABLE_RELOCATABLE=OFF`.

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -7,7 +7,7 @@ version = v"0.1.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://poppler.freedesktop.org/poppler-20.10.0.tar.xz", "434ecbbb539c1a75955030a1c9b24c7b58200b7f68d2e4269e29acf2f8f13336"),
+    ArchiveSource("https://poppler.freedesktop.org/poppler-0.87.0.tar.xz", "6f602b9c24c2d05780be93e7306201012e41459f289b8279a27a79431ad4150e"),
 	GitSource("https://github.com/paveloom-j/PDFHighlights.jl.git", "42fc05c2b21d7359d13a38daf252045a79ab55c5"),
 ]
 
@@ -35,6 +35,7 @@ cmake -DCMAKE_INSTALL_PREFIX=$prefix \
     -DENABLE_QT5=OFF \
     -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
     -DWITH_GObjectIntrospection=OFF \
+    -DENABLE_RELOCATABLE=OFF \
     ..
 
 make -j${nproc}
