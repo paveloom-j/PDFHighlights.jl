32b52576dce1a7c4b2573c2b20b72c44121547c8
Pavel Sobolev <paveloom@mail.ru>
2020-10-26 14:46:22 +0300

    Try the new `Poppler_jll` binaries (no deps).

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -7,39 +7,11 @@ version = v"0.1.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://poppler.freedesktop.org/poppler-0.87.0.tar.xz", "6f602b9c24c2d05780be93e7306201012e41459f289b8279a27a79431ad4150e"),
 	GitSource("https://github.com/paveloom-j/PDFHighlights.jl.git", "42fc05c2b21d7359d13a38daf252045a79ab55c5"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/poppler-*/
-
-# Create link ${bindir} before starting.  `OpenJPEGTargets.cmake` will try to
-# look for some executables in `sys-root/usr/local/bin`
-# ln -s ${bindir} /opt/${target}/${target}/sys-root/usr/local/bin
-export CXXFLAGS="-I${prefix}/include/openjpeg-2.3"
-
-if [[ "${target}" == "${MACHTYPE}" ]]; then
-    # When building for the host platform, the system libexpat is picked up
-    rm /usr/lib/libexpat.so*
-fi
-
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix \
-    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_GTK_TESTS=OFF \
-    -DENABLE_CMS=lcms2 \
-    -DENABLE_GLIB=ON \
-    -DENABLE_QT5=OFF \
-    -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
-    -DWITH_GObjectIntrospection=OFF \
-    ..
-
-make -j${nproc}
-make install
-
 cd $WORKSPACE/srcdir/PDFHighlights.jl/deps/
 
 CC="gcc"
@@ -87,17 +59,17 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency("Xorg_xorgproto_jll"),
-    # Dependency("Poppler_jll"),
-    Dependency("Cairo_jll"),
-    Dependency("Fontconfig_jll"),
+    # BuildDependency("Xorg_xorgproto_jll"),
+    Dependency("Poppler_jll"),
+    # Dependency("Cairo_jll"),
+    # Dependency("Fontconfig_jll"),
     # Dependency("GTK3_jll"),
-    Dependency("Glib_jll"),
-    Dependency("JpegTurbo_jll"),
-    Dependency("Libtiff_jll"),
-    Dependency("OpenJpeg_jll"),
+    # Dependency("Glib_jll"),
+    # Dependency("JpegTurbo_jll"),
+    # Dependency("Libtiff_jll"),
+    # Dependency("OpenJpeg_jll"),
     # Dependency("gdk_pixbuf_jll"),
-    Dependency("libpng_jll"),
+    # Dependency("libpng_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
